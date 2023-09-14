7e16baa3fe06ae2795e3e06e0e65779a2f17585f
Bhubon Sutradhar Nil <sdbhubon@gmail.com>
2023-09-14 23:52:43 +0600

    Optional param and nested route

---
+++ src/components/User.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { reactive, onBeforeMount } from 'vue';
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+</script>
+<template>
+    <div>
+        <h2>User Page</h2>
+        <p v-if="$route.params.id" >User Id: {{ $route.params.id }}</p>
+        <p v-else >Unspecified User</p>
+        <RouterView></RouterView>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
--- src/router/router.js
+++ src/router/router.js
@@ -4,6 +4,9 @@ import About from '../components/About.vue';
 import Contact from '../components/Contact.vue';
 import Post from '../components/Post.vue';
 import Posts from '../components/Posts.vue';
+import User from '../components/User.vue';
+import UserProfile from '../components/UserProfile.vue';
+import NotFound from '../components/NotFound.vue';
 
 
 const routes = [
@@ -12,6 +15,15 @@ const routes = [
     { path: '/contact', component: Contact, name: 'contact' },
     { path: '/post', component: Posts },
     { path: '/post/:id', component: Post, name: 'post' },
+    {
+        path: '/user/:id?',
+        component: User,
+        name: 'user',
+        children: [
+            { path: "profile", component: UserProfile }
+        ]
+    },
+    { path: '/:pathMatch(.*)*', component: NotFound },
 ];
 
 const router = createRouter({
